c4b8336efca763eedfdb22a88d6c0da1c345870f
wangyt <wangyt_zzu@163.com>
2019-11-27 17:08:46 +0800

    android camera2 api 预览&获取预览数据

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 * AudioTrack 播放 wav 音频（static mode & stream mode）
 * android camera2 api 使用 参考（https://github.com/WangYantao/android-camera-demos）
 * android Extractor 解封装 mp4 文件为 aac 和 h264 & Muxer 封装 video track 为 mp4文件
+* android camera2 api 预览&获取预览数据
--- app/src/main/java/com/demo/avdemos/camera/CameraActivity.java
+++ app/src/main/java/com/demo/avdemos/camera/CameraActivity.java
@@ -1,15 +1,204 @@
 package com.demo.avdemos.camera;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
 
 import com.demo.avdemos.R;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class CameraActivity extends AppCompatActivity {
+
+    private static final String TAG = "CameraActivity";
+
+    SurfaceView surfaceView;
+
+    CameraManager cameraManager;
+    CameraCharacteristics characteristics;
+    String cameraId;
+
+    ImageReader outputReader;
+    Size outputSize;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        initView();
+    }
+
+    private void initView() {
+        surfaceView = findViewById(R.id.surfaceView);
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if (holder == null) {
+                    return;
+                }
+
+                openCamera();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
+    }
+
+    private void openCamera() {
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        setCameraIdAndCharacs();
+
+        setOutputSize();
+
+        setReader();
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+            try {
+                cameraManager.openCamera(cameraId, new CameraDevice.StateCallback() {
+                    @Override
+                    public void onOpened(@NonNull final CameraDevice camera) {
+                        if (camera == null){
+                            return;
+                        }
+
+                        final Surface previewSurface = surfaceView.getHolder().getSurface();
+
+                        try {
+                            camera.createCaptureSession(Arrays.asList(previewSurface, outputReader.getSurface()), new CameraCaptureSession.StateCallback() {
+                                @Override
+                                public void onConfigured(@NonNull CameraCaptureSession session) {
+                                    try {
+                                        CaptureRequest.Builder builder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                                        builder.addTarget(previewSurface);
+                                        builder.addTarget(outputReader.getSurface());
+                                        CaptureRequest request = builder.build();
+
+                                        session.setRepeatingRequest(request, new CameraCaptureSession.CaptureCallback() {
+                                            @Override
+                                            public void onCaptureProgressed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureResult partialResult) {
+                                                super.onCaptureProgressed(session, request, partialResult);
+                                            }
+
+                                            @Override
+                                            public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                                                super.onCaptureCompleted(session, request, result);
+                                            }
+                                        }, null);
+                                    } catch (CameraAccessException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                @Override
+                                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+                                }
+                            }, null);
+                        } catch (CameraAccessException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onDisconnected(@NonNull CameraDevice camera) {
+
+                    }
+
+                    @Override
+                    public void onError(@NonNull CameraDevice camera, int error) {
+
+                    }
+                }, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }else {
+            Toast.makeText(this, "请打开相机权限", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+    private void setCameraIdAndCharacs() {
+        try {
+            for (String cid : cameraManager.getCameraIdList()) {
+                characteristics = cameraManager.getCameraCharacteristics(cid);
+                int cameraFacing = characteristics.get(CameraCharacteristics.LENS_FACING);
+                if (cameraFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    cameraId = cid;
+                    break;
+                }
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void setOutputSize() {
+        StreamConfigurationMap configs = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+        List<Size> sizes = Arrays.asList(configs.getOutputSizes(SurfaceTexture.class));
+        Collections.sort(sizes, new Comparator<Size>() {
+            @Override
+            public int compare(Size o1, Size o2) {
+                return o2.getWidth() * o2.getHeight() - o1.getWidth() * o1.getHeight();
+            }
+        });
+        outputSize = sizes.get(0);
+        Log.e(TAG, "setOutputSize: w = " + outputSize.getWidth() + "; h = " + outputSize.getHeight());
+    }
+
+    private void setReader(){
+        outputReader = ImageReader.newInstance(outputSize.getWidth(), outputSize.getHeight(), ImageFormat.YUV_420_888, 2);
+        outputReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                Image image = reader.acquireLatestImage();
+                if (image != null){
+                    ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                    byte[] data = new byte[buffer.remaining()];
+                    buffer.get(data);
+                    Log.e(TAG, "onImageAvailable: data-size:" + data.length * 1.0 / 1024 / 1024 + "M");
+                    image.close();
+                }
+            }
+        }, null);
     }
 }
